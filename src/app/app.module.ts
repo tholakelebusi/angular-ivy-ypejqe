9a7d21814504ecf6870bb20a278dad7a98c69bfa
tholakelebusi <tholakelebusi@gmail.com>
2020-09-08 00:00:26 +0100

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+    delete(name)
+    {
+      const index=this.todolist.indexOf(name);
+      if(index>-1)
+      {
+        this.todolist.splice(index,1);
+      }
+    }
+
+
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule],
+  declarations: [ AppComponent, HelloComponent,DialogComponent ],
+  bootstrap:    [ AppComponent ],
+
 })
 export class AppModule { }
